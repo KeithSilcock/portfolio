e48c6a86b22f484c1c7da3766c43a12367804b1b
Keith Silcock <Silcockk@gmail.com>
2019-07-10 16:52:46 -0700

    adding recaptcha

--- myPortfolio/myPHPmailer/mail_handler.php
+++ myPortfolio/myPHPmailer/mail_handler.php
@@ -9,7 +9,7 @@ require_once('email_config.php');
 require('./PHPMailer/PHPMailerAutoload.php');
 require_once "recaptchalib.php";
 
-
+echo(json_encode($_SERVER));
 
 $secret = $_SERVER['recaptcha'];
 echo $secret;
