46c2ca6710533052ecfaebf82a367e3f83e9c1ee
Keith Silcock <Silcockk@gmail.com>
2019-07-10 17:36:06 -0700

    adding recaptcha

--- myPortfolio/myPHPmailer/mail_handler.php
+++ myPortfolio/myPHPmailer/mail_handler.php
@@ -1,8 +1,8 @@
 
 <?php
-ini_set('display_errors', 'On');
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL | E_STRICT);
+// ini_set('display_errors', 'On');
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL | E_STRICT);
 
 
 require_once('email_config.php');
